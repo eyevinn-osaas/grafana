a4502abe616287b337683979754198d1fa95f22f
Jonas Birmé <jonas.birme@eyevinn.se>
2025-04-30 10:32:39 +0200

    feat: allow embed origins

--- osc-entrypoint.sh
+++ osc-entrypoint.sh
@@ -7,4 +7,11 @@ fi
 echo "server-root-url: $GF_SERVER_ROOT_URL"
 export GF_SERVER_HTTP_PORT=8080
 
+if [ ! -z "$OSC_ALLOW_EMBED_ORIGINS" ]; then
+  export GF_SECURITY_ALLOW_EMBEDDING=true
+  export GF_CORS_ENABLED=true
+  export GF_CORS_ALLOWED_ORIGINS=$OSC_EMBED_ORIGINS
+fi
+# export GF_AUTH_ANONYMOUS_ENABLED=true
+
 exec /run.sh
